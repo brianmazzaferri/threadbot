dc02a637ab9c31e7180378d62e6daddb298a5767
Glitch (slack-demo-template) <none>
2020-08-26 16:23:57 +0000

    🛫🌏 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# slackboltdemotemplate
-Template for building Slack demo apps using Bolt, defaulted 
+# Brian's Basic Bolt
+
+This is a basic take on Bolt for Javascript,
+pre setup for distribution via the Bolt OAuth flow,
+using a simple nedb database
---
+++ app.js
@@ -0,0 +1,71 @@
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+const Datastore = require("nedb"), //(require in the database)
+  // Security note: the database is saved to the file `datafile` on the local filesystem. It's deliberately placed in the `.data` directory
+  // which doesn't get copied if someone remixes the project.
+  db = new Datastore({ filename: ".data/datafile", autoload: true }); //initialize the database
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'my-state-secret',
+  scopes: ['SCOPE1','SCOPE2'], //add scopes here
+  installationStore: {
+    storeInstallation: (installation) => {
+      console.log("INSTALLATION:");
+      console.log(installation);
+      return db.insert(installation, (err, newDoc) => {
+        if (err) console.log("There's a problem with the database ", err);
+        else if (newDoc) console.log("installation insert completed");
+      });
+    },
+    fetchInstallation: async (InstallQuery) => {
+      console.log("FETCH:");
+      console.log(InstallQuery);
+      let incomingteam = InstallQuery.teamId;
+      let result = await queryOne({"team.id":InstallQuery.teamId});
+      console.log(result);
+      return result;
+    },
+  },
+});
+
+//LISTENERS GO HERE
+
+
+
+//BOILERPLATE BELOW HERE
+
+//look up any one document from a query string
+function queryOne(query) {
+  return new Promise((resolve, reject) => {
+    db.findOne(query, (err, docs) => {
+      if (err) console.log("There's a problem with the database: ", err);
+      else if (docs) console.log(query + " queryOne run successfully.");
+      resolve(docs);
+    });
+  });
+}
+
+//print the whole database (for testing)
+function printDatabase() {
+  db.find({}, (err, data) => {
+    if (err) console.log("There's a problem with the database: ", err);
+    else if (data) console.log(data);
+  });
+}
+
+//clear out the database
+function clearDatabase(team,channel) {
+  db.remove({team:team, channel:channel}, { multi: true }, function(err) {
+    if (err) console.log("There's a problem with the database: ", err);
+    else console.log("database cleared");
+  });
+}
+(async () => {
+  // boilerplate to start the app
+  await app.start(process.env.PORT || 3000);
+  //printDatabase();
+  console.log("⚡️ Bolt app is running!");
+})();
